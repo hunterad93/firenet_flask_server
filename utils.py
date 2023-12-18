1406a26f14ecebaa9b1b74fed0e1aeb563c718c0
hunterad93 <129822878+hunterad93@users.noreply.github.com>
2023-12-18 16:00:20 -0700

    fix global var issue

--- utils.py
+++ utils.py
@@ -2,28 +2,31 @@ from google.cloud import bigquery
 
 
 
-# Create a BigQuery client
-bigquery_client = bigquery.Client()
-
-# Initialize geojson_data as a dictionary
-geojson_data = {}
-
-def update_geojson(table_dict):
-    global geojson_data
-    # Use the authenticated BigQuery client to fetch the row
-    for table_id, column_name in table_dict.items():
-        geojson_data[table_id] = download_blob(table_id, column_name, bigquery_client)
-
 def get_geojson_data(table_dict):
-    global geojson_data
-    for table_id in table_dict:
-        if table_id not in geojson_data:
-            update_geojson(table_dict)
-            break
+    """
+    Given a dictionary of table IDs and column names, return a dictionary of geojson data for each table.
+    Fetches the most recent geojson data from each table and updates the geojson_data dictionary.
+
+    :param table_dict: Dictionary where keys are BigQuery table IDs and values are column names
+    :return: Dictionary where keys are BigQuery table IDs and values are geojson data
+    """
+    # Create a BigQuery client
+    bigquery_client = bigquery.Client()
+    geojson_data = {}
+    for table_id, column_name in table_dict.items():
+        # Use the authenticated BigQuery client to fetch the row
+        geojson_data[table_id] = query_most_recent_geojson(table_id, column_name, bigquery_client)
     return geojson_data
 
-def download_blob(table_id, column_name, bigquery_client):
-    """Fetches a row with the latest prediction_date from the BigQuery table."""
+def query_most_recent_geojson(table_id, column_name, bigquery_client):
+    """
+    Fetches a row with the latest prediction_date from the specified BigQuery table and returns the value of the specified column.
+
+    :param table_id: The ID of the BigQuery table to fetch data from
+    :param column_name: The name of the column to return the value from
+    :param bigquery_client: The authenticated BigQuery client to use for fetching data
+    :return: The value of the specified column from the row with the latest prediction_date in the specified table
+    """
     # Specify the project and dataset
     project = "firenet-99"
     dataset = "geojson_predictions"
