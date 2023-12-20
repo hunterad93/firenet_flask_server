b02a829f2bac80a24d0365c9ab4ed7513e285df2
hunterad93 <129822878+hunterad93@users.noreply.github.com>
2023-12-19 20:17:45 -0700

    fix refresh button

--- static/js/map.js
+++ static/js/map.js
@@ -1,21 +1,33 @@
-// Initialize the map with a default view
-var map = L.map('map').setView([35.0, -99.0], 4);
-// Add a tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-}).addTo(map);
+// Global variables for map, layer control, and layers
+var map;
+var layerControl;
+var goesmaskLayer;
+var viirsMaskLayer;
 
 // Function to fetch data and update map
 function updateMap() {
+    // Initialize the map and add tile layer if they don't exist
+    if (typeof map === 'undefined') {
+        map = L.map('map');
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 17,
+        }).addTo(map);
+    }
+
+    // Recenter and reset zoom level of the map
+    map.setView([35.0, -99.0], 4);
+
     fetch('/geojson')
         .then(response => response.json())
         .then(data => {
-            // If layers exist, remove them
+            // If layers exist, remove them from map and layer control
             if (typeof goesmaskLayer !== 'undefined') {
                 map.removeLayer(goesmaskLayer);
+                layerControl.removeLayer(goesmaskLayer);
             }
             if (typeof viirsMaskLayer !== 'undefined') {
                 map.removeLayer(viirsMaskLayer);
+                layerControl.removeLayer(viirsMaskLayer);
             }
 
             // Add the new data to the map as layers
@@ -38,16 +50,16 @@ function updateMap() {
                 }
             }).addTo(map);
 
+            // If layer control exists, remove it
+            if (typeof layerControl !== 'undefined') {
+                layerControl.remove();
+            }
+
             // Add the layers to the layers control
             var overlayLayers = {
                 "GOES Mask": goesmaskLayer,
                 "VIIRS Mask": viirsMaskLayer
             };
-
-            // If layer control exists, remove it
-            if (typeof layerControl !== 'undefined') {
-                layerControl.remove();
-            }
 
             // Add new layer control
             layerControl = L.control.layers({}, overlayLayers).addTo(map);
